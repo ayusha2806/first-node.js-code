e1f7bd9e64fcdec50f33207e04e9278b485ceba0
ayusha2806 <anandayush2806@gmail.com>
2023-11-18 22:39:06 +0530

    Writing and reading from a file in node.js

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 const http = require('http');
-
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
 
-    res.setHeader('Content-type', 'text/html');
-
-    if (req.url === '/home') {
-        res.write('<html><head><title>Welcome Home</title></head><body><h1>Welcome home</h1></body></html>');
-    } else if (req.url === '/about') {
-        res.write('<html><head><title>About Us</title></head><body><h1>Welcome to About Us page</h1></body></html>');
-    } else if (req.url === '/node') {
-        res.write('<html><head><title>Node.js Project</title></head><body><h1>Welcome to my Node.js project</h1></body></html>');
-    } else {
-        res.write('<html><head><title>My First Page</title></head><body><h1>Hello From my node.js server</h1></body></html>');
+    if (req.url === '/') {
+        res.write('<html><head><title>Welcome Home</title></head><body>');
+        res.write('<form action="/message" method="POST">');
+        res.write('Enter Your Message<input type="text" name="message">');
+        res.write('<button type="submit">Submit</button>');
+        res.write('</form>');
+        res.write('</body></html>');
+        res.end();
     }
 
-    res.end();
+
+    if (req.url === "/message" && req.method === "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1]; 
+
+            res.write(`<p>Entered Message: ${message}</p>`);
+
+            res.write('<html><head><title>Welcome Home</title></head><body>');
+            res.write('<form action="/message" method="POST">');
+            res.write('Enter Your Message<input type="text" name="message">');
+            res.write('<button type="submit">Submit</button>');
+            res.write('</form>');
+            res.write('</body></html>');
+            res.end();
+
+            
+
+
+            res.end();
+        });
+    }
 });
 
 server.listen(8000, () => {
